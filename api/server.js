55dc9d8268b4fd248ff9c362df2ffcf5e9b054d2
Rafael Molina <seissaysseis@users.noreply.github.com>
2021-02-23 18:48:52 -0700

    table seeded

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 
+const usersRouter = require('./users/users-router');
+
 const server = express();
 
 server.use(helmet());
@@ -10,9 +12,10 @@ server.use(express.json());
 server.use(cors());
 
 
+server.use('/api/users', usersRouter);
+
 server.get('/', (req, res) => {
     res.json({api: "up"})
 });
 
 module.exports = server;
-
